ecc07fa5801b5cc7268af214975b454482bc2af0
Flavio da Mata <flaviodamataflv@gmail.com>
2023-11-07 15:20:49 -0300

    user-test-database

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express'
+import { v4 } from 'uuid'
+
+import User from './app/models/User'
 
 const routes = new Router()
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Progamador do Futuro',
+    email: 'futuro@progamação.com',
+    password_hash: '123456abc',
+  })
+  return response.json(user)
 })
 
 export default routes
